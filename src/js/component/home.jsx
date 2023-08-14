7eace90502e72ffa919e95a226c6bfd4cf2f94a9
Yoselin Calvetti <138071248+YoselinCalvetti@users.noreply.github.com>
2023-08-14 15:36:52 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+	
+
+    const handleInputChange = event => {
+        setInputValue(event.target.value);
+    };
+
+    const handleKeyPress = event => {
+        if (event.key === "Enter" && inputValue.trim() !== "") {
+            setTodos(prevTodos => [...prevTodos, inputValue]);
+            setInputValue("");
+        }
+    };
+
+	const handleDeleteItem = indexToDelete => {
+        setTodos(prevTodos => prevTodos.filter((_, index) => index !== indexToDelete));
+    };
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+        <div className="container">
+            <h1 className="text-center">todos</h1>
+            <ul className="list-group">
+                <li className="list-group-item d-flex justify-content-between align-items-center">
+                    <input
+                        type="text"
+                        className="form-control border-0"
+                        placeholder="What needs to be done?"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        onKeyPress={handleKeyPress}
+                    />
+                </li>
+                {todos.map((todo, index) => (
+                    <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+                        {todo}
+                        <button className="btn btn-outline-danger" onClick={() => handleDeleteItem(index)}>
+                            <i className="fas fa-times"></i>
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <div>{todos.length} item left</div>
+        </div>
+    );
 };
 
 export default Home;
+
+
+
+
+
+
